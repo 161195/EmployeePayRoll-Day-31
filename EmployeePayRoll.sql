3390f2045af6060f335940fc5d5e33ecb4638856
“mayuri” <mayurisalunkhe01@gmail.com>
2021-11-19 23:49:16 +0530

    adding EmployeePayRoll SQL DB to remote

--- EmployeePayRoll.sql
+++ EmployeePayRoll.sql
@@ -1,3 +1,11 @@
 -----UC1 Create Database-------
 create database Payroll_Services_Database
 use Payroll_Services_Database;
+-----UC2 CreateTable--------
+CREATE TABLE employee_payroll 
+(
+	id int IDENTITY (1,1) PRIMARY KEY,
+	name varchar(200),
+	salary float,
+	startDate date
+);	 
